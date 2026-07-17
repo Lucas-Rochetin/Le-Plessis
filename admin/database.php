ff4216cb6e7f0de50cddd9e8ec0d9936c71cbc7c
NEC TRAITEUR <nectraiteur@MacBook-Pro.local>
2026-07-17 10:56:15 +0200

    Created database file to connect to the db

---
+++ admin/database.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "localhost";
+$dbname = "le_plessis_dev";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    } catch(PDOException $e){
+    die($e->getMessage());
+}
